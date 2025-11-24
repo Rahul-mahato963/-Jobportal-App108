76a5a9b1c3654d69a71f23e3c4bfde7b60357aa8
Rahul-mahato963 <rahulmahaseth146@gmail.com>
2025-11-24 22:03:45 +0530

    Update useGetCompanies.jsx

--- frontend/src/hooks/useGetCompanies.jsx
+++ frontend/src/hooks/useGetCompanies.jsx
@@ -1,22 +1,28 @@
 import { setCompanies } from "@/redux/companySlice";
 import axios from "axios";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-const api=import.meta.env.VITE_API_URL;
+import { useDispatch } from "react-redux";
+
+const api = import.meta.env.VITE_API_URL;
 
 const useGetCompanies = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        const fetchCompany = async () => {
-            try {
-                axios.defaults.withCredentials = true;
-                const res = await axios.get(`${api}/api/company/getcompany`);
-                dispatch(setCompanies(res.data.companies));
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchCompany();
-    }, []);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchCompany = async () => {
+      try {
+        axios.defaults.withCredentials = true;
+        const res = await axios.get(`${api}/api/company/getcompany`);
+
+        dispatch(setCompanies(res.data?.companies || [])); 
+      } catch (error) {
+        console.log(error);
+        dispatch(setCompanies([])); 
+      }
+    };
+
+    fetchCompany();
+  }, []);
 };
+
 export default useGetCompanies;
